ea13b91cb9f4615b451f031ed647108ace6054f5
Djebreds <refiahmad.djebreds@gmail.com>
2022-07-22 04:46:05 +0700

    routing admin panel

---
+++ resources/views/admin/certificates/index.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layouts.main')
+@section('title', 'Data Certificates | Basicschool')
+@section('content')
+    <h1>ini index Certificate</h1>
+@endsection
---
+++ resources/views/admin/courseLevels/index.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layouts.main')
+@section('title', 'Course Levels | Basicschool')
+@section('content')
+    <h1>ini index Course Levels</h1>
+@endsection
